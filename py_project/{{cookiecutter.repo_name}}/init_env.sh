455ec7bb84d80ddda70d96e07acc372042d9d446
Vyacheslav Yermakov <vyacheslav.yermakov@exactprosystems.com>
2022-07-13 02:09:00 +0300

    Introduce pip-compile requirements system

--- py_project/{{cookiecutter.repo_name}}/init_env.sh
+++ py_project/{{cookiecutter.repo_name}}/init_env.sh
@@ -1,1 +1,12 @@
-pip install --upgrade pip
+echo install pip-compile-multi
+pip install pip-compile-multi
+
+echo run pip-compile-multi
+pip-compile-multi
+
+echo install all requirements
+pip install -U -r requirements/all.txt
+
+# Setup pre-commit and pre-push hooks
+#pipenv run pre-commit install -t pre-commit
+#pipenv run pre-commit install -t pre-push
